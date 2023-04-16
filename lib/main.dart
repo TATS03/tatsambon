04ae9ec2cd0d5db445510fa2a3a69d50fcac4e01
TATS03 <warranotats910@gmail.com>
2023-04-16 02:12:23 +0100

    first release off contact apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hello world apps',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'hello flutter'),
+      title: ' TATS Contacts App',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late List<Contact> _contacts;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _getContacts();
+  }
+
+  // demande la permission d'accéder aux contacts
+  Future<void> _requestContactPermission() async {
+    final PermissionStatus status = await Permission.contacts.request();
+    if (status.isGranted) {
+      _getContacts();
+    } else if (status.isDenied) {
+     // ignore: use_build_context_synchronously
+     showDialog(
+          context: context,
+          builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("acces refuser"),
+            content: Text("acces refuser relancer l\'apli"),
+            actions: <Widget>[
+              TextButton(
+                child: Text("fermer l\'apli"),
+                onPressed: () {
+                   SystemNavigator.pop();
+                },
+              ),
+              TextButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                             },
+        ),
+      ],
+    );
+  },
+);
+
+    } else if (status.isPermanentlyDenied) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("acces refuser"),
+            content: Text("autoriser lacces dans parametre"),
+            actions: <Widget>[
+              TextButton(
+                child: Text("fermer l\'apli"),
+                onPressed: () {
+                   SystemNavigator.pop();
+                },
+              ),
+      ],
+    );
+  },
+);
+    }
+  }
+
+  // récupère la liste des contacts
+  void _getContacts() async {
+    Iterable<Contact> contacts = await ContactsService.getContacts();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _contacts = contacts.toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('TATS Contacts App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "hello world"
+      body: _contacts == null
+          ? Center(
+              child: ElevatedButton (
+                onPressed: _requestContactPermission,
+                child: Text('Demander la permission d\'accéder aux contacts'),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _contacts.length,
+              itemBuilder: (BuildContext context, int index) {
+                Contact contact = _contacts[index];
+                return ListTile(
+                  leading: contact.avatar != null
+                      ? CircleAvatar(
+                           child: Icon(Icons.person),
+                        )
+                      : CircleAvatar(
+                          child: Text(contact.initials()),
+                        ),
+                  title: Text(contact.displayName ?? ''),
+                  // subtitle: Text(contact.phones.isNotEmpty
+                  //     ? contact.phones.first.value
+                  //     : ''),
+                );
+              },
             ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme.of(context).textTheme.headlineMedium,
-            // ),
-          ],
-        ),
-      ),
-     // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
